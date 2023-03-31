83c87d75ff96ca228c61dfa49a6afd1c1de871e8
Allen Lewis <allenlewis32@gmail.com>
2023-03-31 15:43:11 +0530

    Add project files.

---
+++ Assessment2-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace Assessment2_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine()!;
+            Console.Write("Enter number of agents: ");
+            int nAgents = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the names of other people: ");
+            string others = Console.ReadLine()!;
+            Console.WriteLine(License(name, nAgents, others));
+        }
+        public static int License(string name, int nAgents, string others)
+        {
+            List<string> allNames = new() { name };
+            foreach (string n in others.Split(" "))
+            {
+                allNames.Add(n);
+            }
+            allNames.Sort();
+            int ind = allNames.IndexOf(name);
+            int batch = ind / nAgents + 1;
+            int time = batch * 20;
+            return time;
+        }
+    }
+}
---
+++ Assessment2-2Tests/ProgramTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assessment2_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2_2.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void LicenseTest_A()
+        {
+            Assert.AreEqual(Program.License("Eric", 2, "Adam Caroline Rebecca Frank"), 40);
+        }
+        [TestMethod()]
+        public void LicenseTest_B()
+        {
+            Assert.AreEqual(Program.License("Zebediah", 1, "Bob Jim Becky Pat"), 100);
+        }
+        [TestMethod()]
+        public void LicenseTest_C()
+        {
+            Assert.AreEqual(Program.License("Aaron", 3, "Jane Max Olivia Sam"), 20);
+        }
+    }
+}
